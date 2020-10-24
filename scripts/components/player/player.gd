386762e35987a7fe9e223727ae394beaa60088fe
Вячеслав <gslavikse@gmail.com>
2020-10-24 20:13:45 +0300

    Декомпозиция управления
    Вынесены функциональные методы управления персонажем в отдельные файлы.

---
+++ scripts/components/player/player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+const FLOOR := Vector2.UP
+var velocity := Vector2.ZERO;
+
+var move := preload('./move.gd').new()
+var jump := preload('./jump.gd').new()
+
+onready var sprite := $Sprite as Sprite
+
+# TODO Уменьшение / увеличение персонажа для прохождения уровней в разных местах (интерполяция)
+
+#warning-ignore:unused_argument
+func _physics_process(delta: float) -> void:
+    moving()
+    jumping()
+
+    sprite_flip()
+
+    #warning-ignore:return_value_discarded
+    move_and_slide(velocity, FLOOR)
+
+
+func moving() -> void:
+    velocity.x = move.moving(velocity.x)
+
+    if is_on_wall():
+        velocity.x = 0
+
+
+func jumping() -> void:
+    if is_on_floor():
+        velocity.y = jump.jumping(velocity.y)
+
+    if is_on_ceiling():
+        velocity.y = jump.ceiling(velocity.y)
+
+    velocity.y = jump.continuous_jumping(velocity.y)
+
+
+func sprite_flip() -> void:
+    if velocity.x != 0:
+        sprite.flip_h = velocity.x < 0
