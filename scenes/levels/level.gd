9382c309617b55c2e03155b15fbff8954ea471f1
slavikse <slavikse@ya.ru>
2022-06-14 22:17:05 +0300

    ads

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -70,27 +70,27 @@ onready var is_character_not_added := true
 # 30 - 1
 
 func _ready() -> void:
-    if GlobalController.RERUNS.get(GlobalController.current_level_number):
-        tips_rerun_node.text = str(GlobalController.RERUNS[GlobalController.current_level_number])
+	if GlobalController.RERUNS.get(GlobalController.current_level_number):
+		tips_rerun_node.text = str(GlobalController.RERUNS[GlobalController.current_level_number])
 
 
 func _on_RerunTime_timeout() -> void:
-    tips_rerun_node.hide()
+	tips_rerun_node.hide()
 
 
 func external_can_activate_exit() -> void:
-    if closed_doors == 0:
-        return
+	if closed_doors == 0:
+		return
 
-    closed_doors -= 1
+	closed_doors -= 1
 
-    if closed_doors == 0:
-        exit_node.external_open()
+	if closed_doors == 0:
+		exit_node.external_open()
 
 
 func external_game_over() -> void:
-    if is_character_not_added:
-        is_character_not_added = false
+	if is_character_not_added:
+		is_character_not_added = false
 
-        var dead_player_node := DeadPlayerScene.instance() as DeadPlayer
-        call_deferred('add_child', dead_player_node)
+		var dead_player_node := DeadPlayerScene.instance() as DeadPlayer
+		call_deferred('add_child', dead_player_node)
